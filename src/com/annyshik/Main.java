e073a3cd7cd3b8fa4dc1e67aa696b6d4bfd9a134
Anny Shik <anny.shik21@gmail.com>
2021-01-06 01:40:54 +0200

    create commit for 4st task

--- src/com/annyshik/Main.java
+++ src/com/annyshik/Main.java
@@ -5,78 +5,98 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static void task4() {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите кол-во строк массива: ");
-        int n = scan.nextInt(); //вместо mass1.length
-        System.out.println("Введите кол-во столбцов: ");
-        int m = scan.nextInt(); // вместо mass1[0].length
-        int mass1[][] = new int[n][m];
-/***Заполнение двумерного массива случайными числами*/
-        Random random = new Random();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                mass1[i][j] = random.nextInt(100); //в скобках указать максимальное значение ячейки
-            }
-        }
-
-/*** вывод всего двумерного массива с каждой строки */
-        int sum = 0;
-        int sumSum = 0;
-        for (int[] printMass : mass1) {
-            System.out.println(Arrays.deepToString(new int[][]{printMass}) + " ");
-
-/***суммирование каждого элемента массива*/
-            for (int num : printMass) {
-                sum = sum + num;
-            }
-            //System.out.println("Сумма элементов этого массива равна: " + sum);
-        }
-/***суммирование всех сумм массивов*/
-        for (int num = 0; num <= sum; num++) {
-            sumSum = sumSum + num;
-        }
-        System.out.println("Сумма элементов матрицы равна: " + sumSum);
-
-    }
-
-    public static void task2() {
-        //Вывести все элементы 5-й строки. Вывести элементы 2-го столбца массива.
+    public static int[][] returnMassive() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите кол-во строк массива: ");
-        int n = sc.nextInt();
+        int n = sc.nextInt(); //вместо mass.length
         System.out.println("Введите кол-во столбцов: ");
-        int m = sc.nextInt();
-        int[][] mass2 = new int[n][m];
+        int m = sc.nextInt(); // вместо mass[0].length
+        int mass[][] = new int[n][m];
+        return initializeRandomMassive(mass, m, n);
+    }
 
+    public static int[][] initializeRandomMassive(int mass[][], int m, int n) {
+        /***Заполнение двумерного массива случайными числами*/
         Random random = new Random();
-        System.out.println("Вывод массива: ");
-
         for (int i = 0; i < n; i++) {
             System.out.println();
             for (int j = 0; j < m; j++) {
-                mass2[i][j] = random.nextInt(100);
-                System.out.print(mass2[i][j] + " ");
-            }
-        }
-        System.out.println("");
-
-
-        System.out.println("Выводим последнюю строку массива: ");
-
-        for (int i = n - 1; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(mass2[i][j] + " ");
+                mass[i][j] = random.nextInt(10); //в скобках указать максимальное значение ячейки
+                System.out.print(mass[i][j] + " ");
             }
             System.out.println("");
-            System.out.println("Выводим последнего элемента строки: " + mass2[n - 1][m - 1]);
+        }
+        return mass;
+    }
+
+    public static void task1() {
+        int arr[][] = returnMassive();
+        System.out.println("Число в правом верхнем углу: " + arr[0][5]);
+        System.out.println("Число в левом нижнем углу: " + arr[5][0]);
+    }
+
+    public static void task2(int mass[][], int m, int n) {
+        //Вывести все элементы 5-й строки n. Вывести элементы 2-го столбца массива m.
+        System.out.println("Выводим все элементы 5-й строки: ");
+        for (int i = n - 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(mass[5][j] + " ");
+            }
+            System.out.println("");
+            System.out.println("Выводим все элементы 2-го столбца массива: " + mass[i][2]);
         }
     }
 
+    public static int getSummOfString(int mass[][]) {
+/*** вывод всего двумерного массива с каждой новой строки */
+        int sum = 0;
+        for (int[] printMass : mass) {
+            System.out.println(Arrays.deepToString(new int[][]{printMass}) + " ");
+/***суммирование каждого элемента массива*/
+            for (int num : printMass) {
+                sum += num;
+            }
+            //System.out.println("Сумма элементов этого массива равна: " + sum);
+        }
+        return sum;
+    }
+
+    public static void task4(int sum, int sumSum) {
+        int arr[][] = returnMassive();
+
+        int arraySumma = getSummOfString(arr);
+/***суммирование всех сумм массивов*/
+        for (int num = 0; num <= sum; num++) {
+            sumSum += num;
+        }
+        System.out.println("Сумма элементов матрицы равна: " + sumSum);
+    }
+
+//    public static void task3(int mass[][], int sum, int n, int m) {
+//        int maxString = (sum < 0) ? -sum : sum;
+//        // или
+//
+//        // Переменной maxString задаем минимальное значение.
+//        int maxString = 0;
+//        // Перебираем все элементы массива.
+//        for (int i = 0; i < n; i++) {
+//            System.out.println();
+//            for (int j = 0; j < m; j++) {
+//                // Переменной maxString с помощью метода Math.maxString() присваиваем максимальное значение
+//                // путем выбора наибольшего из двух значений ("старого" значения maxString и значения элемента).
+//                maxString = Math.max(maxString, mass[i][j]);
+//            }
+//            System.out.println("");
+//        }
+//
+//        // Выводим на экран наибольшее число массива mass.
+//        System.out.println("Максимальный элемент в массиве mass: " + maxString);
+//    }
 
     public static void main(String[] args) {
-       // task4();
-        task4();
+        returnMassive();
+        //task1();
+        //task4();
     }
 }
